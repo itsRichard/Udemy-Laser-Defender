a1b7645bb707554a10043828dad3ff6e42f073d3
itsRichard <richardtlee@gmail.com>
2016-04-30 09:45:05 -0700

    Update to Unity 5

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name){
 		Debug.Log ("New Level load: " + name);
-		Application.LoadLevel (name);
+        // Application.LoadLevel (name);    Deprecated for Unity 5
+        SceneManager.LoadScene(name);
 	}
 
 	public void QuitRequest(){
 		Debug.Log ("Quit requested");
-		Application.Quit ();
+        // Application.Quit ();
+        Application.Quit();
 	}
 
 }
